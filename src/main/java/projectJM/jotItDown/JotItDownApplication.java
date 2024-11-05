ab4294764c37741a3a3f4e7283b9af9fafe6895b
jinnieusLab <kitty14904@naver.com>
2024-11-05 16:25:37 -0600

    :sparkles: [feat] 프로젝트 초기 세팅, db 설정

---
+++ src/main/java/projectJM/jotItDown/JotItDownApplication.java
@@ -0,0 +1,13 @@
+package projectJM.jotItDown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JotItDownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JotItDownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projectJM/jotItDown/JotItDownApplicationTests.java
@@ -0,0 +1,13 @@
+package projectJM.jotItDown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JotItDownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
