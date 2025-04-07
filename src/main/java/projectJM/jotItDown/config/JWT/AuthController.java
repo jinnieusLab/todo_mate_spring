c822ecac52a9f070bcc19508fa4041e446d367a4
jinnieusLab <kitty14904@naver.com>
2025-04-07 18:14:47 +0900

    :lipstick: [style] Access Token 재발급 시 응답 메시지 수정

--- src/main/java/projectJM/jotItDown/config/JWT/AuthController.java
+++ src/main/java/projectJM/jotItDown/config/JWT/AuthController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import projectJM.jotItDown.apiPayload.code.status.SuccessStatus;
 
 @RestController
 @RequestMapping("/auth")
@@ -19,6 +18,6 @@ public class AuthController {
     @PostMapping("/refresh")
     public ResponseEntity<String> createNewAccessToken (HttpServletRequest request, HttpServletResponse response) {
         authService.handleExpiredAccessToken(request, response);
-        return ResponseEntity.ok(SuccessStatus._OK.getMessage());
+        return ResponseEntity.ok("Access Token 재발급 성공");
     }
 }
